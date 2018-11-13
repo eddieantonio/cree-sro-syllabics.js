abcff858f6610ec23befb2aa311612a5bc7117dc
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-12 20:14:48 -0700

    Fix remaining Standard.JS style issues.

--- cree_sro_syllabics.js
+++ cree_sro_syllabics.js
@@ -4,7 +4,7 @@ var DEFAULT_HYPHENS = '\u202f'
 var CONSONANT = '[ptkcshmnyw]|th'
 var STRICT_VOWEL = '[êioaîôâ]'
 var VOWEL = `${STRICT_VOWEL}|[eēī'ōā]`
-var sro_pattern = new RegExp(`
+var sroPattern = new RegExp(`
     ((?:${CONSONANT})w?)-(${STRICT_VOWEL}) |
     thê|thi|tho|tha|thî|thô|thâ                            |th|
     wê |wi |wo |wa |wî |wô |wâ                             |w |
@@ -20,7 +20,7 @@ var sro_pattern = new RegExp(`
     ê|i|î|o|ô|a|â|
     -
 `)
-var sro2syllabics_lookup = { 'ê': 'ᐁ', 'i': 'ᐃ', 'î': 'ᐄ', 'o': 'ᐅ', 'ô': 'ᐆ', 'a': 'ᐊ', 'â': 'ᐋ', 'wê': 'ᐍ', 'wi': 'ᐏ', 'wî': 'ᐑ', 'wo': 'ᐓ', 'wô': 'ᐕ', 'wa': 'ᐘ', 'wâ': 'ᐚ', 'w': 'ᐤ', 'p': 'ᑊ', 'pê': 'ᐯ', 'pi': 'ᐱ', 'pî': 'ᐲ', 'po': 'ᐳ', 'pô': 'ᐴ', 'pa': 'ᐸ', 'pâ': 'ᐹ', 'pwê': 'ᐻ', 'pwi': 'ᐽ', 'pwî': 'ᐿ', 'pwo': 'ᑁ', 'pwô': 'ᑃ', 'pwa': 'ᑅ', 'pwâ': 'ᑇ', 't': 'ᐟ', 'tê': 'ᑌ', 'ti': 'ᑎ', 'tî': 'ᑏ', 'to': 'ᑐ', 'tô': 'ᑑ', 'ta': 'ᑕ', 'tâ': 'ᑖ', 'twê': 'ᑘ', 'twi': 'ᑚ', 'twî': 'ᑜ', 'two': 'ᑞ', 'twô': 'ᑠ', 'twa': 'ᑢ', 'twâ': 'ᑤ', 'k': 'ᐠ', 'kê': 'ᑫ', 'ki': 'ᑭ', 'kî': 'ᑮ', 'ko': 'ᑯ', 'kô': 'ᑰ', 'ka': 'ᑲ', 'kâ': 'ᑳ', 'kwê': 'ᑵ', 'kwi': 'ᑷ', 'kwî': 'ᑹ', 'kwo': 'ᑻ', 'kwô': 'ᑽ', 'kwa': 'ᑿ', 'kwâ': 'ᒁ', 'c': 'ᐨ', 'cê': 'ᒉ', 'ci': 'ᒋ', 'cî': 'ᒌ', 'co': 'ᒍ', 'cô': 'ᒎ', 'ca': 'ᒐ', 'câ': 'ᒑ', 'cwê': 'ᒓ', 'cwi': 'ᒕ', 'cwî': 'ᒗ', 'cwo': 'ᒙ', 'cwô': 'ᒛ', 'cwa': 'ᒝ', 'cwâ': 'ᒟ', 'm': 'ᒼ', 'mê': 'ᒣ', 'mi': 'ᒥ', 'mî': 'ᒦ', 'mo': 'ᒧ', 'mô': 'ᒨ', 'ma': 'ᒪ', 'mâ': 'ᒫ', 'mwê': 'ᒭ', 'mwi': 'ᒯ', 'mwî': 'ᒱ', 'mwo': 'ᒳ', 'mwô': 'ᒵ', 'mwa': 'ᒷ', 'mwâ': 'ᒹ', 'n': 'ᐣ', 'nê': 'ᓀ', 'ni': 'ᓂ', 'nî': 'ᓃ', 'no': 'ᓄ', 'nô': 'ᓅ', 'na': 'ᓇ', 'nâ': 'ᓈ', 'nwê': 'ᓊ', 'nwa': 'ᓌ', 'nwâ': 'ᓎ', 's': 'ᐢ', 'sê': 'ᓭ', 'si': 'ᓯ', 'sî': 'ᓰ', 'so': 'ᓱ', 'sô': 'ᓲ', 'sa': 'ᓴ', 'sâ': 'ᓵ', 'swê': 'ᓷ', 'swi': 'ᓹ', 'swî': 'ᓻ', 'swo': 'ᓽ', 'swô': 'ᓿ', 'swa': 'ᔁ', 'swâ': 'ᔃ', 'y': 'ᕀ', 'yê': 'ᔦ', 'yi': 'ᔨ', 'yî': 'ᔩ', 'yo': 'ᔪ', 'yô': 'ᔫ', 'ya': 'ᔭ', 'yâ': 'ᔮ', 'ywê': 'ᔰ', 'ywi': 'ᔲ', 'ywî': 'ᔴ', 'ywo': 'ᔶ', 'ywô': 'ᔸ', 'ywa': 'ᔺ', 'ywâ': 'ᔼ', 'th': 'ᖮ', 'thê': 'ᖧ', 'thi': 'ᖨ', 'thî': 'ᖩ', 'tho': 'ᖪ', 'thô': 'ᖫ', 'tha': 'ᖬ', 'thâ': 'ᖭ', 'l': 'ᓬ', 'r': 'ᕒ', 'h': 'ᐦ', 'hk': 'ᕽ' }
+var sro2syllabicsLookup = { 'ê': 'ᐁ', 'i': 'ᐃ', 'î': 'ᐄ', 'o': 'ᐅ', 'ô': 'ᐆ', 'a': 'ᐊ', 'â': 'ᐋ', 'wê': 'ᐍ', 'wi': 'ᐏ', 'wî': 'ᐑ', 'wo': 'ᐓ', 'wô': 'ᐕ', 'wa': 'ᐘ', 'wâ': 'ᐚ', 'w': 'ᐤ', 'p': 'ᑊ', 'pê': 'ᐯ', 'pi': 'ᐱ', 'pî': 'ᐲ', 'po': 'ᐳ', 'pô': 'ᐴ', 'pa': 'ᐸ', 'pâ': 'ᐹ', 'pwê': 'ᐻ', 'pwi': 'ᐽ', 'pwî': 'ᐿ', 'pwo': 'ᑁ', 'pwô': 'ᑃ', 'pwa': 'ᑅ', 'pwâ': 'ᑇ', 't': 'ᐟ', 'tê': 'ᑌ', 'ti': 'ᑎ', 'tî': 'ᑏ', 'to': 'ᑐ', 'tô': 'ᑑ', 'ta': 'ᑕ', 'tâ': 'ᑖ', 'twê': 'ᑘ', 'twi': 'ᑚ', 'twî': 'ᑜ', 'two': 'ᑞ', 'twô': 'ᑠ', 'twa': 'ᑢ', 'twâ': 'ᑤ', 'k': 'ᐠ', 'kê': 'ᑫ', 'ki': 'ᑭ', 'kî': 'ᑮ', 'ko': 'ᑯ', 'kô': 'ᑰ', 'ka': 'ᑲ', 'kâ': 'ᑳ', 'kwê': 'ᑵ', 'kwi': 'ᑷ', 'kwî': 'ᑹ', 'kwo': 'ᑻ', 'kwô': 'ᑽ', 'kwa': 'ᑿ', 'kwâ': 'ᒁ', 'c': 'ᐨ', 'cê': 'ᒉ', 'ci': 'ᒋ', 'cî': 'ᒌ', 'co': 'ᒍ', 'cô': 'ᒎ', 'ca': 'ᒐ', 'câ': 'ᒑ', 'cwê': 'ᒓ', 'cwi': 'ᒕ', 'cwî': 'ᒗ', 'cwo': 'ᒙ', 'cwô': 'ᒛ', 'cwa': 'ᒝ', 'cwâ': 'ᒟ', 'm': 'ᒼ', 'mê': 'ᒣ', 'mi': 'ᒥ', 'mî': 'ᒦ', 'mo': 'ᒧ', 'mô': 'ᒨ', 'ma': 'ᒪ', 'mâ': 'ᒫ', 'mwê': 'ᒭ', 'mwi': 'ᒯ', 'mwî': 'ᒱ', 'mwo': 'ᒳ', 'mwô': 'ᒵ', 'mwa': 'ᒷ', 'mwâ': 'ᒹ', 'n': 'ᐣ', 'nê': 'ᓀ', 'ni': 'ᓂ', 'nî': 'ᓃ', 'no': 'ᓄ', 'nô': 'ᓅ', 'na': 'ᓇ', 'nâ': 'ᓈ', 'nwê': 'ᓊ', 'nwa': 'ᓌ', 'nwâ': 'ᓎ', 's': 'ᐢ', 'sê': 'ᓭ', 'si': 'ᓯ', 'sî': 'ᓰ', 'so': 'ᓱ', 'sô': 'ᓲ', 'sa': 'ᓴ', 'sâ': 'ᓵ', 'swê': 'ᓷ', 'swi': 'ᓹ', 'swî': 'ᓻ', 'swo': 'ᓽ', 'swô': 'ᓿ', 'swa': 'ᔁ', 'swâ': 'ᔃ', 'y': 'ᕀ', 'yê': 'ᔦ', 'yi': 'ᔨ', 'yî': 'ᔩ', 'yo': 'ᔪ', 'yô': 'ᔫ', 'ya': 'ᔭ', 'yâ': 'ᔮ', 'ywê': 'ᔰ', 'ywi': 'ᔲ', 'ywî': 'ᔴ', 'ywo': 'ᔶ', 'ywô': 'ᔸ', 'ywa': 'ᔺ', 'ywâ': 'ᔼ', 'th': 'ᖮ', 'thê': 'ᖧ', 'thi': 'ᖨ', 'thî': 'ᖩ', 'tho': 'ᖪ', 'thô': 'ᖫ', 'tha': 'ᖬ', 'thâ': 'ᖭ', 'l': 'ᓬ', 'r': 'ᕒ', 'h': 'ᐦ', 'hk': 'ᕽ' }
 var WORD_INITIAL = `
     [ptkcmnsyh]w? |
     (?:th|[rl]) |
@@ -59,8 +59,8 @@ var END_WORD = `
 var WORD = `
     ${BEGIN_WORD} ${MORPHEME} (?: (?:${CODA})?-${MORPHEME})* ${END_WORD}
 `
-var word_pattern = new RegExp(WORD, 'i')
-var full_stop_pattern = new RegExp(`
+var wordPattern = new RegExp(WORD, 'i')
+var fullStopPattern = new RegExp(`
     (?<=[\\u1400-\\u167f])[.] |
     \\A[.]\\Z
 `)
@@ -72,39 +72,39 @@ var sro2syllabics = function (sro, hyphens, sandhi) {
   if (typeof sandhi === 'undefined' || (sandhi != null && sandhi.hasOwnProperty('__kwargtrans__'))) {
     sandhi = true
   }
-  var transliterate_word = function (match) {
-    return transcode_sro_word_to_syllabics(match.group(0), hyphens, sandhi)
+  var transliterateWord = function (match) {
+    return transcodeSROWordToSyllabics(match.group(0), hyphens, sandhi)
   }
-  var transliteration = word_pattern.sub(transliterate_word, nfc(sro))
-  return full_stop_pattern.sub('᙮', transliteration)
+  var transliteration = wordPattern.sub(transliterateWord, nfc(sro))
+  return fullStopPattern.sub('᙮', transliteration)
 }
-var transcode_sro_word_to_syllabics = function (sro_word, hyphen, sandhi) {
-  var to_transcribe = sro_word.lower().translate(TRANSLATE_ALT_FORMS)
-  var lookup = Object.new(sro2syllabics_lookup)
+var transcodeSROWordToSyllabics = function (sroWord, hyphen, sandhi) {
+  var toTranscribe = sroWord.lower().translate(TRANSLATE_ALT_FORMS)
+  var lookup = Object.new(sro2syllabicsLookup)
   Object.assign(lookup, { '-': hyphen })
   var parts = []
-  var match = sro_pattern.match(to_transcribe)
+  var match = sroPattern.match(toTranscribe)
   while (match) {
     var __left0__ = match.groups()
     var onset = __left0__[0]
     var vowel = __left0__[1]
     if (sandhi && onset !== null) {
       var syllable = onset + vowel
-      var next_syllable_pos = match.end()
+      var nextSyllablePos = match.end()
     } else if (onset !== null) {
-      syllable = (onset == 'w' ? 'w' : onset.rstrip('w'))
-      next_syllable_pos = syllable.length
+      syllable = (onset === 'w' ? 'w' : onset.rstrip('w'))
+      nextSyllablePos = syllable.length
     } else {
       syllable = match.group(0)
-      next_syllable_pos = match.end()
+      nextSyllablePos = match.end()
     }
     let syllabic = lookup[syllable]
     parts.append(syllabic)
-    to_transcribe = to_transcribe.__getslice__(next_syllable_pos, null, 1)
-    match = sro_pattern.match(to_transcribe)
+    toTranscribe = toTranscribe.__getslice__(nextSyllablePos, null, 1)
+    match = sroPattern.match(toTranscribe)
   }
-  if (parts.__getslice__(-(2), null, 1) == ['ᐦ', 'ᐠ']) {
-    parts.__setslice__(-(2), null, null, [sro2syllabics_lookup['hk']])
+  if (parts.__getslice__(-(2), null, 1) === ['ᐦ', 'ᐠ']) {
+    parts.__setslice__(-(2), null, null, [sro2syllabicsLookup['hk']])
   }
   return ''.join(parts)
 }
@@ -113,39 +113,39 @@ function nfc (text) {
   return text.normalize('NFC')
 }
 
-var syllabics2sro_lookup = (function () {
+var syllabics2sroLookup = (function () {
   let lookup = {}
-  for (var [sro, syl] of Object.entries(sro2syllabics_lookup)) {
+  for (var [sro, syl] of Object.entries(sro2syllabicsLookup)) {
     lookup[syl] = sro
   }
   return lookup
 })()
 
-Object.assign(syllabics2sro_lookup, { 'ᐝ': 'y', '᙮': '.', 'ᑦ': 'm', 'ᕁ': 'hk', 'ᐩ': 'y', '\u202f': '-' })
-var SYLLABICS_TO_SRO = _maketrans(syllabics2sro_lookup)
+Object.assign(syllabics2sroLookup, { 'ᐝ': 'y', '᙮': '.', 'ᑦ': 'm', 'ᕁ': 'hk', 'ᐩ': 'y', '\u202f': '-' })
+var SYLLABICS_TO_SRO = _maketrans(syllabics2sroLookup)
 var SYLLABIC_WITH_DOT = { 'ᐁ': 'ᐍ', 'ᐃ': 'ᐏ', 'ᐄ': 'ᐑ', 'ᐅ': 'ᐓ', 'ᐆ': 'ᐕ', 'ᐊ': 'ᐘ', 'ᐋ': 'ᐚ', 'ᐯ': 'ᐻ', 'ᐱ': 'ᐽ', 'ᐲ': 'ᐿ', 'ᐳ': 'ᑁ', 'ᐴ': 'ᑃ', 'ᐸ': 'ᑅ', 'ᐹ': 'ᑇ', 'ᑌ': 'ᑘ', 'ᑎ': 'ᑚ', 'ᑏ': 'ᑜ', 'ᑐ': 'ᑞ', 'ᑑ': 'ᑠ', 'ᑕ': 'ᑢ', 'ᑖ': 'ᑤ', 'ᑫ': 'ᑵ', 'ᑭ': 'ᑷ', 'ᑮ': 'ᑹ', 'ᑯ': 'ᑻ', 'ᑰ': 'ᑽ', 'ᑲ': 'ᑿ', 'ᑳ': 'ᒁ', 'ᒉ': 'ᒓ', 'ᒋ': 'ᒕ', 'ᒌ': 'ᒗ', 'ᒍ': 'ᒙ', 'ᒎ': 'ᒛ', 'ᒐ': 'ᒝ', 'ᒑ': 'ᒟ', 'ᒣ': 'ᒭ', 'ᒥ': 'ᒯ', 'ᒦ': 'ᒱ', 'ᒧ': 'ᒳ', 'ᒨ': 'ᒵ', 'ᒪ': 'ᒷ', 'ᒫ': 'ᒹ', 'ᓀ': 'ᓊ', 'ᓇ': 'ᓌ', 'ᓈ': 'ᓎ', 'ᓭ': 'ᓷ', 'ᓯ': 'ᓹ', 'ᓰ': 'ᓻ', 'ᓱ': 'ᓽ', 'ᓲ': 'ᓿ', 'ᓴ': 'ᔁ', 'ᓵ': 'ᔃ', 'ᔦ': 'ᔰ', 'ᔨ': 'ᔲ', 'ᔩ': 'ᔴ', 'ᔪ': 'ᔶ', 'ᔫ': 'ᔸ', 'ᔭ': 'ᔺ', 'ᔮ': 'ᔼ' }
-var final_dot_pattern = (function () {
-  let without_dot = Object.keys(SYLLABIC_WITH_DOT).join('')
-  return new RegExp(`([${without_dot}])ᐧ`)
+var finalDotPattern = (function () {
+  let withoutDot = Object.keys(SYLLABIC_WITH_DOT).join('')
+  return new RegExp(`([${withoutDot}])ᐧ`)
 }())
-var circumflex_to_macrons = _maketrans('êîôâ', 'ēīōā')
-var syllabics2sro = function (syllabics, produce_macrons) {
-  if (typeof produce_macrons === 'undefined' || (produce_macrons != null && produce_macrons.hasOwnProperty('__kwargtrans__'))) {
-    produce_macrons = false
+var circumflexToMacrons = _maketrans('êîôâ', 'ēīōā')
+var syllabics2sro = function (syllabics, produceMacrons) {
+  if (typeof produceMacrons === 'undefined' || (produceMacrons != null && produceMacrons.hasOwnProperty('__kwargtrans__'))) {
+    produceMacrons = false
   }
-  var fix_final_dot = function (match) {
+  var fixFinalDot = function (match) {
     return SYLLABIC_WITH_DOT[match.group(1)]
   }
-  var normalized = final_dot_pattern.sub(fix_final_dot, syllabics)
-  var sro_string = normalized.translate(SYLLABICS_TO_SRO)
-  if (produce_macrons) {
-    return sro_string.translate(circumflex_to_macrons)
+  var normalized = finalDotPattern.sub(fixFinalDot, syllabics)
+  var sroString = normalized.translate(SYLLABICS_TO_SRO)
+  if (produceMacrons) {
+    return sroString.translate(circumflexToMacrons)
   }
-  return sro_string
+  return sroString
 }
 
 function _maketrans () {
-  new Error('not implemented')
+  throw Error('not implemented')
 }
 
 /* Node exports. */
