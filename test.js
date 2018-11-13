49c1150859124362400ae952058a8482033c5efe
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-12 23:38:09 -0700

    Test roundtrip (no new implementation required).

--- test.js
+++ test.js
@@ -2,11 +2,6 @@ import test from 'ava'
 
 let { sro2syllabics, syllabics2sro } = require('./')
 
-function convertRoundTrip (t, sro, syllabics) {
-  t.is(sro2syllabics(sro), syllabics)
-  t.is(syllabics2sro(syllabics), sro)
-}
-
 /* Basic, full word tests. */
 test('"acimosis" → syllabics', convertRoundTrip, 'acimosis', 'ᐊᒋᒧᓯᐢ')
 test('"atahk" → syllabics', convertRoundTrip, 'atahk', 'ᐊᑕᕽ')
@@ -14,3 +9,16 @@ test('"mêriy" → syllabics', convertRoundTrip, 'mêriy', 'ᒣᕒᐃᕀ')
 test('"wîstihkêw" → syllabics', convertRoundTrip, 'wîstihkêw', 'ᐑᐢᑎᐦᑫᐤ')
 test('"nêhiyawêwin" → syllabics', convertRoundTrip, 'nêhiyawêwin', 'ᓀᐦᐃᔭᐍᐏᐣ')
 test('"tirêyl" → syllabics', convertRoundTrip, 'tirêyl', 'ᑎᕒᐁᕀᓬ')
+
+/**
+ * Test macro that tests SRO → syllabics, syllabics → SRO,
+ * SRO → syllabics → SRO, and syllabics → SRO → syllabics.
+ */
+function convertRoundTrip (t, sro, syllabics) {
+  // Test simple conversion first.
+  t.is(sro2syllabics(sro), syllabics)
+  t.is(syllabics2sro(syllabics), sro)
+  // Test roundtrip/inverse
+  t.is(syllabics2sro(sro2syllabics(sro)), sro)
+  t.is(sro2syllabics(syllabics2sro(syllabics)), syllabics)
+}
