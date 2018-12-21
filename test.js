0146de591f6c20ba421b364d2370d8110e13de80
Eddie Antonio Santos <easantos@ualberta.ca>
2018-12-21 15:42:31 -0700

    Autogenerate some titles.

--- test.js
+++ test.js
@@ -22,25 +22,32 @@ let { sro2syllabics, syllabics2sro } = require('./cree-sro-syllabics')
 const COMBINING_CIRCUMFLEX = '\u0302'
 
 /* Create automatic titles for some test cases. */
-convertSemiRoundTrip.title = (_providedTitle, sro, syllabics) =>
-  `${sro} ↔ ${syllabics}`
-convertSemiRoundTripWithMacrons.title = (_providedTitle, sro, syllabics) =>
-  `${sro} ↔ ${syllabics}`
+convertSemiRoundTrip.title = (providedTitle, sro, syllabics) =>
+  providedTitle || `“${sro}” ↔ “${syllabics}”`
+convertSemiRoundTripWithMacrons.title = (providedTitle, sro, syllabics) =>
+  `“${sro}” ↔ “${syllabics}”`
+convertRoundTrip.title = (_providedTitle, sro, syllabics) =>
+  `“${sro}” ⇔ “${syllabics}”`
+convertToSyllabics.title = (comment, sro, syllabics) =>
+  `“${sro}” → “${syllabics}”` + (comment ? ` (${comment})` : '')
+convertToSRO.title = (comment, syllabics, sro) =>
+  `“${syllabics}” → “${sro}” (${comment})`
 
 // ////////////////////////////// TEST CASES ////////////////////////////// //
 
 /* Basic, full word tests. */
-test('"acimosis" → syllabics', convertRoundTrip, 'acimosis', 'ᐊᒋᒧᓯᐢ')
-test('"atahk" → syllabics', convertRoundTrip, 'atahk', 'ᐊᑕᕽ')
-test('"mêriy" → syllabics', convertRoundTrip, 'mêriy', 'ᒣᕒᐃᕀ')
-test('"wîstihkêw" → syllabics', convertRoundTrip, 'wîstihkêw', 'ᐑᐢᑎᐦᑫᐤ')
-test('"nêhiyawêwin" → syllabics', convertRoundTrip, 'nêhiyawêwin', 'ᓀᐦᐃᔭᐍᐏᐣ')
-test('"tirêyl" → syllabics', convertRoundTrip, 'tirêyl', 'ᑎᕒᐁᕀᓬ')
+test(convertRoundTrip, 'acimosis', 'ᐊᒋᒧᓯᐢ')
+test(convertRoundTrip, 'atahk', 'ᐊᑕᕽ')
+test(convertRoundTrip, 'mêriy', 'ᒣᕒᐃᕀ')
+test(convertRoundTrip, 'wîstihkêw', 'ᐑᐢᑎᐦᑫᐤ')
+test(convertRoundTrip, 'nêhiyawêwin', 'ᓀᐦᐃᔭᐍᐏᐣ')
+test(convertRoundTrip, 'tirêyl', 'ᑎᕒᐁᕀᓬ')
 
 /* Spelling relaxation tests. */
-test('"Tân\'si" → syllabics', convertToSyllabics, "Tân'si", 'ᑖᓂᓯ')
-test('"Maskekosihk" → syllabics', convertToSyllabics, 'Maskekosihk', 'ᒪᐢᑫᑯᓯᕽ')
+test(convertToSyllabics, "Tân'si", 'ᑖᓂᓯ')
+test(convertToSyllabics, 'Maskekosihk', 'ᒪᐢᑫᑯᓯᕽ')
 
+// TODO: test as if String.prototype.normalize() does not exist.
 test('Unicode normalization', t => {
   let water = 'nipiy'
   let leaf = 'ni' + COMBINING_CIRCUMFLEX + 'piy'
@@ -62,10 +69,9 @@ test(convertSemiRoundTripWithMacrons, 'yōtinipēstāw', 'ᔫᑎᓂᐯᐢᑖᐤ'
 test(convertSemiRoundTripWithMacrons, 'īkatē', 'ᐄᑲᑌ')
 
 /* Test hyphens */
-test('"paskwâwi-mostos" → syllabics', convertRoundTrip, 'paskwâwi-mostos', 'ᐸᐢᒁᐏ ᒧᐢᑐᐢ')
-test('"amiskwaciy-waskahikan" → syllabics', convertRoundTrip, 'amiskwaciy-waskahikan', 'ᐊᒥᐢᑿᒋᕀ ᐘᐢᑲᐦᐃᑲᐣ')
-test('"kâ-mahihkani-pimohtêt isiyihkâsow" → syllabics', convertRoundTrip,
-  'kâ-mahihkani-pimohtêt isiyihkâsow', 'ᑳ ᒪᐦᐃᐦᑲᓂ ᐱᒧᐦᑌᐟ ᐃᓯᔨᐦᑳᓱᐤ')
+test(convertRoundTrip, 'paskwâwi-mostos', 'ᐸᐢᒁᐏ ᒧᐢᑐᐢ')
+test(convertRoundTrip, 'amiskwaciy-waskahikan', 'ᐊᒥᐢᑿᒋᕀ ᐘᐢᑲᐦᐃᑲᐣ')
+test(convertRoundTrip, 'kâ-mahihkani-pimohtêt isiyihkâsow', 'ᑳ ᒪᐦᐃᐦᑲᓂ ᐱᒧᐦᑌᐟ ᐃᓯᔨᐦᑳᓱᐤ')
 
 test('supplying custom hyphens', t => {
   let customHyphen = '$'
@@ -73,10 +79,10 @@ test('supplying custom hyphens', t => {
 })
 
 /* Test Sandhi */
-test('"osk-âya" → syllabics', convertToSyllabics, 'osk-âya', 'ᐅᐢᑳᔭ')
-test('"miyw-âyâw" → syllabics', convertToSyllabics, 'miyw-âyâw', 'ᒥᔼᔮᐤ')
-test('"pîhc-âyihk" → syllabics', convertToSyllabics, 'pîhc-âyihk', 'ᐲᐦᒑᔨᕽ')
-test('"wîhth-owin" → syllabics', convertToSyllabics, 'wîhth-owin', 'ᐑᐦᖪᐏᐣ')
+test(convertToSyllabics, 'osk-âya', 'ᐅᐢᑳᔭ')
+test(convertToSyllabics, 'miyw-âyâw', 'ᒥᔼᔮᐤ')
+test(convertToSyllabics, 'pîhc-âyihk', 'ᐲᐦᒑᔨᕽ')
+test(convertToSyllabics, 'wîhth-owin', 'ᐑᐦᖪᐏᐣ')
 
 /* Test full-stop */
 test('test full-stop (all Cree)', convertSemiRoundTrip, 'êtî nitisiyihkâson.', 'ᐁᑏ ᓂᑎᓯᔨᐦᑳᓱᐣ᙮')
@@ -85,12 +91,12 @@ test('test full-stop (mostly English)', convertSemiRoundTrip,
 test('test full-stop (multiple)', convertSemiRoundTrip, 'tânisi. êtî nitisiyihkâson. ', 'ᑖᓂᓯ᙮ ᐁᑏ ᓂᑎᓯᔨᐦᑳᓱᐣ᙮ ')
 
 /* Test final-middle dot. */
-test('"ᐋᐧᐱ ᑭᐦᐃᐤ" → SRO', convertToSRO, 'ᐋᐧᐱ ᑭᐦᐃᐤ', 'wâpi kihiw')
+test('final middle-dot', convertToSRO, 'ᐋᐧᐱ ᑭᐦᐃᐤ', 'wâpi kihiw')
 test('"ᐋᐧᐱ ᑭᐦᐃᐤ" → "ᐚᐱ ᑭᐦᐃᐤ"', t => {
   t.is(sro2syllabics(syllabics2sro('ᐋᐧᐱ ᑭᐦᐃᐤ')), 'ᐚᐱ ᑭᐦᐃᐤ')
 })
 /* Regression: https://github.com/eddieantonio/cree-sro-syllabics.js/issues/9 */
-test('"ᐃᐢᑫᐧᐤ ᐊᐱᐦᑕᐃᐧᑯᓯᓴᐣ" -> sro', convertToSRO, 'ᐃᐢᑫᐧᐤ ᐊᐱᐦᑕᐃᐧᑯᓯᓴᐣ', 'iskwêw apihtawikosisan')
+test('Regression: GH-9', convertToSRO, 'ᐃᐢᑫᐧᐤ ᐊᐱᐦᑕᐃᐧᑯᓯᓴᐣ', 'iskwêw apihtawikosisan')
 
 /* Test lookalikes */
 test('lookalike: U+1466 CANADIAN SYLLABICS T', convertLookalike, 'ᐚᐸ\u1466', 'wâpam', 'ᐚᐸᒼ')
@@ -98,20 +104,19 @@ test('lookalike: U+1541 CANADIAN SYLLABICS SAYISI YI', convertLookalike, 'ᓂᐲ
 test('lookalike: U+1429 CANADIAN SYLLABICS FINAL PLUS', convertLookalike, 'ᓂᐱ\u1429', 'nipiy', 'ᓂᐱᕀ')
 
 /* Test th-dialect */
-test('"wīhthēw" → syllabics', convertSemiRoundTripWithMacrons, 'wīhthēw', 'ᐑᐦᖧᐤ')
-test('"nampithi-sīpīhk" → syllabics', convertSemiRoundTripWithMacrons, 'nampithi-sīpīhk', 'ᓇᒼᐱᖨ ᓰᐲᕽ')
-test('"mithomon" → syllabics', convertSemiRoundTripWithMacrons, 'mithomon', 'ᒥᖪᒧᐣ')
-test('"namōtha" → syllabics', convertSemiRoundTripWithMacrons, 'namōtha', 'ᓇᒨᖬ')
-test('"thāhkan" → syllabics', convertSemiRoundTripWithMacrons, 'thāhkan', 'ᖭᐦᑲᐣ')
-test('"namēpith" → syllabics', convertSemiRoundTripWithMacrons, 'namēpith', 'ᓇᒣᐱᖮ')
-test('"thē thi tho tha thī thō thā" → syllabics', convertSemiRoundTripWithMacrons,
-  'thē thi tho tha thī thō thā', 'ᖧ ᖨ ᖪ ᖬ ᖩ ᖫ ᖭ')
+test(convertSemiRoundTripWithMacrons, 'wīhthēw', 'ᐑᐦᖧᐤ')
+test(convertSemiRoundTripWithMacrons, 'nampithi-sīpīhk', 'ᓇᒼᐱᖨ ᓰᐲᕽ')
+test(convertSemiRoundTripWithMacrons, 'mithomon', 'ᒥᖪᒧᐣ')
+test(convertSemiRoundTripWithMacrons, 'namōtha', 'ᓇᒨᖬ')
+test(convertSemiRoundTripWithMacrons, 'thāhkan', 'ᖭᐦᑲᐣ')
+test(convertSemiRoundTripWithMacrons, 'namēpith', 'ᓇᒣᐱᖮ')
+test(convertSemiRoundTripWithMacrons, 'thē thi tho tha thī thō thā', 'ᖧ ᖨ ᖪ ᖬ ᖩ ᖫ ᖭ')
 
 /* Test rare nwV forms */
-test('"nwe nwa nwā" → syllabics', convertToSyllabics, 'nwe nwa nwā', 'ᓊ ᓌ ᓎ')
+test('rare nwV forms', convertToSyllabics, 'nwe nwa nwā', 'ᓊ ᓌ ᓎ')
 
 /* Test adjacent "vowels" */
-test(`"I'm" → syllabics`, convertToSyllabics, "I'm", "I'm")
+test('adjacent "vowels"', convertToSyllabics, "I'm", "I'm")
 
 /* Test short-i elision: https://github.com/eddieantonio/cree-sro-syllabics.js/issues/8 */
 test('Using U+0027 APOSTROPHE', convertToSyllabics, "tan'si", 'ᑕᓂᓯ')
