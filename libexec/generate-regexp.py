cec90bcf34d9778cbc6959d7458307f534c825e4
Eddie Antonio Santos <easantos@ualberta.ca>
2020-05-11 11:14:04 -0600

    style: apply black to generate-regexp.py

--- libexec/generate-regexp.py
+++ libexec/generate-regexp.py
@@ -26,12 +26,13 @@ import re
 
 # Derived from
 # https://github.com/eddieantonio/cree-sro-syllabics/tree/v2018.11.08
-CONSONANT = '[ptkcshmnyw]|th'
-STRICT_VOWEL = '[êioaîôâ]'
+CONSONANT = "[ptkcshmnyw]|th"
+STRICT_VOWEL = "[êioaîôâ]"
 VOWEL = f"{STRICT_VOWEL}|[eēī'’ōā]"
 
 # Match an SRO syllable.
-sro_pattern = re.compile(fr'''
+sro_pattern = re.compile(
+    fr"""
     # A syllable that should be joined under the sandhi rule:
     # We're setting this up so that the onset (consonant and optional w) can
     # be glued together with the vowel. The parts are joined to
@@ -60,7 +61,9 @@ sro_pattern = re.compile(fr'''
     h|l|r|
     ê|i|î|o|ô|a|â|
     -
-''', re.VERBOSE)
+""",
+    re.VERBOSE,
+)
 
 # These regular expressions are intended to strictly match Cree words
 # We want to match *CREE* words, because we want to avoid accidentally
@@ -72,124 +75,179 @@ sro_pattern = re.compile(fr'''
 #
 # For more information, see:
 # https://en.wikipedia.org/wiki/Plains_Cree#Phonotactics
-WORD_INITIAL = r'''
+WORD_INITIAL = r"""
     (?:[ptkcmnsyh]|th)w? |    # consonants that allow 'w' after
     [rl] |  # consonants that don't
     w |
     # can start with no consonant.
-'''
+"""
 
-WORD_MEDIAL = r'''
+WORD_MEDIAL = r"""
     # TODO: there should be a constraint that the constants cannot be
     # duplicated, but capturing groups won't work if these regex
     # snippets are concatenated into bigger regexes.
     (?:[hsmnwy]|th)? (?:[ptkcmnsyh]|th) w? |
     w |
     [yw]? [rl]  # for loan words
-'''
+"""
 
-WORD_FINAL = r'''
+WORD_FINAL = r"""
     [hs]? (?:[ptcksmnwy]|th) |
     h |
     [yw]? [rl]  # for loan word
     |  # can end with no consonant
-'''
+"""
 
 # NOTE: VOWEL is defined way near the top of the file.
 
-CODA = 'th|[hs]?[ptkcmn]|h|s|y|w'
-MORPHEME = fr'''
+CODA = "th|[hs]?[ptkcmn]|h|s|y|w"
+MORPHEME = fr"""
     (?:{WORD_INITIAL}) (?:{VOWEL})
         (?: (?:{WORD_MEDIAL}) (?:{VOWEL}) )*
     (?:{WORD_FINAL})
-'''
+"""
 
 # TODO: DRY these up!
-BEGIN_WORD = r'''
+BEGIN_WORD = r"""
 (?:
         ^  # Either the start of a string; or,
         |  # at the edge of "letters".
         ([^a-zêîôâēī'’ōā])
 )
-'''
-END_WORD = r'''
+"""
+END_WORD = r"""
 (?:
         (?=[^a-zêioaîôâeēī'’ōā]) |
         $
 )
-'''
+"""
 
-WORD = fr'''
+WORD = fr"""
     # CODA before the hyphen to account for Sandhi.
     # It's possible to accept TWO codas using this formulation, but
     # I think that loss of precision is okay.
     {BEGIN_WORD} ({MORPHEME} (?: (?:{CODA})?-{MORPHEME})*) {END_WORD}
-'''
+"""
 word_pattern = re.compile(WORD, re.IGNORECASE | re.VERBOSE)
 
 # This regex prevents matching EVERY period, instead only matching periods
 # after Cree words, or, as an exception, as the only item in a string.
-full_stop_pattern = re.compile(r'''
+full_stop_pattern = re.compile(
+    r"""
     ([\u1400-\u167F])[.] |  # Match a full-stop after syllabics
     ^[.]$                   # or match as the only item.
-''', re.VERBOSE)
+""",
+    re.VERBOSE,
+)
 
 # For use when converting SYLLABIC + FINAL MIDDLE DOT into the syllabic
 # with a 'w'
 SYLLABIC_WITH_DOT = {
-    'ᐁ': 'ᐍ', 'ᐃ': 'ᐏ', 'ᐄ': 'ᐑ', 'ᐅ': 'ᐓ', 'ᐆ': 'ᐕ', 'ᐊ': 'ᐘ', 'ᐋ': 'ᐚ',
-    'ᐯ': 'ᐻ', 'ᐱ': 'ᐽ', 'ᐲ': 'ᐿ', 'ᐳ': 'ᑁ', 'ᐴ': 'ᑃ', 'ᐸ': 'ᑅ', 'ᐹ': 'ᑇ',
-    'ᑌ': 'ᑘ', 'ᑎ': 'ᑚ', 'ᑏ': 'ᑜ', 'ᑐ': 'ᑞ', 'ᑑ': 'ᑠ', 'ᑕ': 'ᑢ', 'ᑖ': 'ᑤ',
-    'ᑫ': 'ᑵ', 'ᑭ': 'ᑷ', 'ᑮ': 'ᑹ', 'ᑯ': 'ᑻ', 'ᑰ': 'ᑽ', 'ᑲ': 'ᑿ', 'ᑳ': 'ᒁ',
-    'ᒉ': 'ᒓ', 'ᒋ': 'ᒕ', 'ᒌ': 'ᒗ', 'ᒍ': 'ᒙ', 'ᒎ': 'ᒛ', 'ᒐ': 'ᒝ', 'ᒑ': 'ᒟ',
-    'ᒣ': 'ᒭ', 'ᒥ': 'ᒯ', 'ᒦ': 'ᒱ', 'ᒧ': 'ᒳ', 'ᒨ': 'ᒵ', 'ᒪ': 'ᒷ', 'ᒫ': 'ᒹ',
-    'ᓀ': 'ᓊ',                                         'ᓇ': 'ᓌ', 'ᓈ': 'ᓎ',
-    'ᓭ': 'ᓷ', 'ᓯ': 'ᓹ', 'ᓰ': 'ᓻ', 'ᓱ': 'ᓽ', 'ᓲ': 'ᓿ', 'ᓴ': 'ᔁ', 'ᓵ': 'ᔃ',
-    'ᔦ': 'ᔰ', 'ᔨ': 'ᔲ', 'ᔩ': 'ᔴ', 'ᔪ': 'ᔶ', 'ᔫ': 'ᔸ', 'ᔭ': 'ᔺ', 'ᔮ': 'ᔼ',
+    "ᐁ": "ᐍ",
+    "ᐃ": "ᐏ",
+    "ᐄ": "ᐑ",
+    "ᐅ": "ᐓ",
+    "ᐆ": "ᐕ",
+    "ᐊ": "ᐘ",
+    "ᐋ": "ᐚ",
+    "ᐯ": "ᐻ",
+    "ᐱ": "ᐽ",
+    "ᐲ": "ᐿ",
+    "ᐳ": "ᑁ",
+    "ᐴ": "ᑃ",
+    "ᐸ": "ᑅ",
+    "ᐹ": "ᑇ",
+    "ᑌ": "ᑘ",
+    "ᑎ": "ᑚ",
+    "ᑏ": "ᑜ",
+    "ᑐ": "ᑞ",
+    "ᑑ": "ᑠ",
+    "ᑕ": "ᑢ",
+    "ᑖ": "ᑤ",
+    "ᑫ": "ᑵ",
+    "ᑭ": "ᑷ",
+    "ᑮ": "ᑹ",
+    "ᑯ": "ᑻ",
+    "ᑰ": "ᑽ",
+    "ᑲ": "ᑿ",
+    "ᑳ": "ᒁ",
+    "ᒉ": "ᒓ",
+    "ᒋ": "ᒕ",
+    "ᒌ": "ᒗ",
+    "ᒍ": "ᒙ",
+    "ᒎ": "ᒛ",
+    "ᒐ": "ᒝ",
+    "ᒑ": "ᒟ",
+    "ᒣ": "ᒭ",
+    "ᒥ": "ᒯ",
+    "ᒦ": "ᒱ",
+    "ᒧ": "ᒳ",
+    "ᒨ": "ᒵ",
+    "ᒪ": "ᒷ",
+    "ᒫ": "ᒹ",
+    "ᓀ": "ᓊ",
+    "ᓇ": "ᓌ",
+    "ᓈ": "ᓎ",
+    "ᓭ": "ᓷ",
+    "ᓯ": "ᓹ",
+    "ᓰ": "ᓻ",
+    "ᓱ": "ᓽ",
+    "ᓲ": "ᓿ",
+    "ᓴ": "ᔁ",
+    "ᓵ": "ᔃ",
+    "ᔦ": "ᔰ",
+    "ᔨ": "ᔲ",
+    "ᔩ": "ᔴ",
+    "ᔪ": "ᔶ",
+    "ᔫ": "ᔸ",
+    "ᔭ": "ᔺ",
+    "ᔮ": "ᔼ",
 }
-final_dot_pattern = re.compile(r'([{without_dot}])ᐧ'.format(
-    without_dot=''.join(SYLLABIC_WITH_DOT.keys())
-))
+final_dot_pattern = re.compile(
+    r"([{without_dot}])ᐧ".format(without_dot="".join(SYLLABIC_WITH_DOT.keys()))
+)
 
 
-def print_javascript_regexp(name: str, python_regex: re.Pattern, flags=''):
+def print_javascript_regexp(name: str, python_regex: re.Pattern, flags=""):
     # The variable declaration first
-    print('const', name, '=', end=' ')
+    print("const", name, "=", end=" ")
 
     if python_regex.flags & re.VERBOSE:
         # Remove comments and whitespace in verbose regexps.
         parts = []
-        for line in python_regex.pattern.split('\n'):
-            regex, _hash, _comment = line.partition('#')
+        for line in python_regex.pattern.split("\n"):
+            regex, _hash, _comment = line.partition("#")
             parts.extend(regex.split())
     else:
         parts = [python_regex.pattern]
 
     # Figure out the JavaScript compatible flags.
     if python_regex.flags & re.IGNORECASE:
-        flags += 'i'
+        flags += "i"
 
     parts = [uniescape(part) for part in parts]
 
     # Print the regex!
-    print('/', *parts, '/', flags, sep='')
+    print("/", *parts, "/", flags, sep="")
 
 
 def uniescape(text: str) -> str:
     """
     Escapes all non-ASCII printable characters with JavaScript Unicode escapes.
     """
+
     def escape(match):
         character = match.group(0)
         assert len(character) == 1
         code_point = ord(character)
         assert code_point <= 0xFFFF
-        return f'\\u{code_point:04X}'
-    return re.sub('[^\u0020-\u007F]', escape, text)
+        return f"\\u{code_point:04X}"
+
+    return re.sub("[^\u0020-\u007F]", escape, text)
 
 
-if __name__ == '__main__':
-    print_javascript_regexp('sroPattern', sro_pattern)
-    print_javascript_regexp('wordPattern', word_pattern, flags='g')
-    print_javascript_regexp('fullStopPattern', full_stop_pattern, flags='g')
-    print_javascript_regexp('finalDotPattern', final_dot_pattern, flags='g')
+if __name__ == "__main__":
+    print_javascript_regexp("sroPattern", sro_pattern)
+    print_javascript_regexp("wordPattern", word_pattern, flags="g")
+    print_javascript_regexp("fullStopPattern", full_stop_pattern, flags="g")
+    print_javascript_regexp("finalDotPattern", final_dot_pattern, flags="g")
